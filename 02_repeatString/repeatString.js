bc330680bdf66488f5b51db099ec740efb817bef
[John Wilkins] <[interruptingvolcano@protonmail.com]>
2022-10-29 14:47:58 -0500

    Update exercises--finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  myStr = string;
+  for (i = 1; i < num; i++) {
 
+    myStr += string;
+  }
+  if (num === 0) {
+    myStr = '';
+  } else if (num < 0) {
+    myStr = 'ERROR';
+  }
+  return myStr;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
