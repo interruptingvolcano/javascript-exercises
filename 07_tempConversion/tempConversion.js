40bc2e3c6534a279c8c7d3700a52f98e65fe8d18
[John Wilkins] <[interruptingvolcano@protonmail.com]>
2022-10-31 12:00:54 -0500

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+function ftoc(f) {
+  tempC = (f - 32) * 5/9;
+  if (tempC - Math.floor(tempC) !== 0) {
+    roundTempC = tempC.toFixed(1);
+    return Number(roundTempC);
+  } else {
+    return Math.trunc(tempC);
+  }
+}
 
-};
-
-const ctof = function() {
-
-};
+function ctof(c) {
+  tempF = (c * 9/5 + 32)
+    if (tempF - Math.floor(tempF) !== 0) {
+      roundTempF = tempF.toFixed(1);
+      return Number(roundTempF);
+    } else {
+      return Math.trunc(tempF);
+    }
+}
 
 // Do not edit below this line
 module.exports = {
